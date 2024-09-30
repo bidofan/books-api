c4b33cd44272b80bec3882a1217f946bd2435d8f
bidofan <alex.bidofan@gmail.com>
2024-09-30 23:00:54 +0300

    Connect to mongoDB on app start

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 import { bookRoutes } from './interface/routes/bookRoutes'
 import { errorHandler } from './interface/middleware/errorHandler'
 
+dotenv.config()
 const app = express()
+const MONGOURL = process.env.MONGO_URL as string
+const PORT = process.env.PORT || 4000
 
 app.use(express.json())
 app.use('/api', bookRoutes)
 app.use(errorHandler)
 
-const PORT = 4000
+mongoose.connect(MONGOURL).then(() => {
+  console.log('Successfully connected to mongoDB')
+})
+.catch((err) => { console.log(err) })
 
 app.listen(PORT, () => {
   console.log('Server runninng on port' + PORT)
